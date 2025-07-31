bbea1ab17b71545f841c29a0f0b464e1430190d4
Moez Mabrouk <131665435+moez-insta@users.noreply.github.com>
2025-07-31 14:29:13 +0200

    Update test_pvc.sh

--- test_pvc.sh
+++ test_pvc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 " > /mnt/aws-test-200de3e68ede40ad-moez-a31c94ef9ef54b5e/file2.txt
+echo "test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 test PVC BY MOEZ2 " > /mnt/testprojpvc-af7b62643072486e-test-0fae16c22bfc4f47/file2.txt
 #cat /mnt/import-66eccab3d9c1483d-premium-test-b1671a25c25d4d7d/file.txt
 #cat /mnt/import-66eccab3d9c1483d-premium-test-b1671a25c25d4d7d/file2.txt
 sleep 100
